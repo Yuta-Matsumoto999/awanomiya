ef81bcc9ccd3a8225e21b684d9c0b4a28a924211
松本雄大 <matsumotoyuudai@matsumotoyuudainoMacBook-Air.local>
2023-07-16 14:55:03 +0900

    create common layout

---
+++ components/layout/layout.tsx
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import { ReactNode } from "react";
+import Header from "../common/Header";
+import Footer from "../common/Footer";
+import Link from "next/link";
+
+interface Props {
+    children: ReactNode;
+    title: string
+}
+
+export default function Layout({children, title}: Props) {
+    return (
+        <div className="flex justify-center items-center flex-col min-h-screen text-gray-600 text-sm font-mono">
+            <Head>
+                <title>{title}</title>
+            </Head>
+            <Header />
+            <main className="flex flex-1 justify-center items-center flex-col w-screen">
+                {children}
+            </main>
+            <Footer />
+        </div>
+    )
+}
