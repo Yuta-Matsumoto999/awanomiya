000581e4ca06caf34473c2020fd635824dd6c402
松本雄大 <matsumotoyuudai@matsumotoyuudainoMacBook-Air.local>
2023-07-16 15:10:40 +0900

    create news page

---
+++ pages/news/index.tsx
@@ -0,0 +1,9 @@
+import Layout from "@/components/layout/layout";
+
+export default function News() {
+    return (
+        <Layout title="News">
+            <p className="text-3xl font-bold">NEWS</p>
+        </Layout>
+    )
+}
