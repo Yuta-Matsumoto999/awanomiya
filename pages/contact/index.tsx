60b3c2c7d29ce90545f3e114e1cd604926ccfa9e
松本雄大 <matsumotoyuudai@matsumotoyuudainoMacBook-Air.local>
2023-07-16 15:10:14 +0900

    create contact page

---
+++ pages/contact/index.tsx
@@ -0,0 +1,9 @@
+import Layout from "@/components/layout/layout";
+
+export default function Contact() {
+    return (
+        <Layout title="Contact">
+            <p className="text-4xl font-bold">CONTACT</p>
+        </Layout>
+    )
+}
