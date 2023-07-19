3cce6bafaed74a4ddf8bbad0afb1c623843abee4
松本雄大 <matsumotoyuudai@matsumotoyuudainoMacBook-Air.local>
2023-07-19 16:26:18 +0900

    Delete unnecessary imports

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -2,7 +2,6 @@ import Head from "next/head";
 import { ReactNode } from "react";
 import Header from "../common/Header";
 import Footer from "../common/Footer";
-import Link from "next/link";
 
 interface Props {
     children: ReactNode;
