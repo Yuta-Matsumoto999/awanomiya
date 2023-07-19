6ae7dd98e27336461d82847592271847ba91c3ee
松本雄大 <matsumotoyuudai@matsumotoyuudainoMacBook-Air.local>
2023-07-19 16:27:04 +0900

    Change the text on the top page

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <Layout title='Home'>
-        <p className='text-4xl text-center'>Welcome awanomiya!!!</p>
+        <p className='text-3xl text-center font-bold'>Welcome!!!</p>
     </Layout>
   )
 }
