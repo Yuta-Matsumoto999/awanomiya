ee2181e4f66d5be838abb87a7f1b1bae562442cb
松本雄大 <matsumotoyuudai@matsumotoyuudainoMacBook-Air.local>
2023-07-19 16:23:44 +0900

    Add blog details query

--- constants/WpGraphQlConst.ts
+++ constants/WpGraphQlConst.ts
@@ -1,6 +1,6 @@
 export class WpGraphQlPostConst {
     static list = `query PostListQuery {
-        posts {
+        posts(first: 1000) {
             edges {
                 node {
                     id
@@ -21,6 +21,39 @@ export class WpGraphQlPostConst {
                             sourceUrl
                         }
                     }
+                }
+            }
+        }
+    }`
+
+    static one = `query PostOneQuery($id: ID!) {
+        post(id: $id, idType: SLUG) {
+            categories {
+                edges {
+                    node {
+                        name
+                        slug
+                    }
+                }
+            }
+            date
+            content
+            featuredImage {
+                node {
+                    sourceUrl
+                }
+            }
+            id
+            slug
+            title
+        }
+    }`
+
+    static allSlug = `query PostListQuery {
+        posts(first: 10000) {
+            edges {
+                node {
+                    slug
                 }
             }
         }
